ab6062d7fa2fa3d2b423bfebbe002db1613086be
Firdous Nath <firdous.nath@alpha-vector.com>
2025-01-28 07:18:49 +0530

    feat: Add localization

---
+++ flutter_forge/lib/presentation/localization/title_hi.i69n.dart
@@ -0,0 +1,53 @@
+// ignore_for_file: unused_element, unused_field, camel_case_types, annotate_overrides, prefer_single_quotes
+// GENERATED FILE, do not edit!
+import 'package:i69n/i69n.dart' as i69n;
+import 'title.i69n.dart';
+
+String get _languageCode => 'hi';
+String get _localeName => 'hi';
+
+String _plural(int count,
+        {String? zero,
+        String? one,
+        String? two,
+        String? few,
+        String? many,
+        String? other}) =>
+    i69n.plural(count, _languageCode,
+        zero: zero, one: one, two: two, few: few, many: many, other: other);
+String _ordinal(int count,
+        {String? zero,
+        String? one,
+        String? two,
+        String? few,
+        String? many,
+        String? other}) =>
+    i69n.ordinal(count, _languageCode,
+        zero: zero, one: one, two: two, few: few, many: many, other: other);
+String _cardinal(int count,
+        {String? zero,
+        String? one,
+        String? two,
+        String? few,
+        String? many,
+        String? other}) =>
+    i69n.cardinal(count, _languageCode,
+        zero: zero, one: one, two: two, few: few, many: many, other: other);
+
+class Title_hi extends Title {
+  const Title_hi();
+  String get welcome => "फ़्लटर फोर्ज़ में आपका स्वागत है";
+  Object operator [](String key) {
+    var index = key.indexOf('.');
+    if (index > 0) {
+      return (this[key.substring(0, index)]
+          as i69n.I69nMessageBundle)[key.substring(index + 1)];
+    }
+    switch (key) {
+      case 'welcome':
+        return welcome;
+      default:
+        return super[key];
+    }
+  }
+}
